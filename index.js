4ba1519916aee89e3c23126d77792902126bb660
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-14 13:26:05 +0400

    feat: add cors

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./routers/authRouter');
 const usersRouter = require('./routers/userRouter');
+var cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 const uri = "mongodb+srv://admin:admin@itransition-task4.oncn2vx.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
