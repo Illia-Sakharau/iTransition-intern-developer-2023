8a47f4d57179d3950cda7e753662cd409e388061
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-16 15:53:57 +0400

    feat: add unblock functionality

--- task-4/src/components/3-organism/bulkBar/components/UnblockButton.tsx
+++ task-4/src/components/3-organism/bulkBar/components/UnblockButton.tsx
@@ -1,13 +1,33 @@
 import { FC, ReactElement } from 'react';
-import { Button } from 'react-bootstrap';
 import { UnlockFill } from 'react-bootstrap-icons';
+import { useSetUsersStatusMutation } from '../../../../API/UserAPI';
+import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
+import RectButton from '../../../1-atom/button/RectButton';
+import {selectedUsersSlice} from '../../../../store/reducers/selectedUsersSlice';
 
 const UnblockButton: FC = (): ReactElement => {
+  const [blockUsers, {isLoading}] = useSetUsersStatusMutation();
+  const {selectedUsers} = useAppSelector(state=>state.selectedUsersSlice);
+  const dispath = useAppDispatch();
+  const { clearSelectedList } = selectedUsersSlice.actions;
+  
+  const clickHandler = async () => {
+    await blockUsers({
+      emails: selectedUsers,
+      isActive: true
+    });
+    dispath(clearSelectedList())
+  }
 
   return (
-    <Button variant="secondary" size="lg">
+    <RectButton 
+      variant="secondary"
+      size="lg"
+      onClick={clickHandler}
+      isLoading={isLoading}
+    >
       <UnlockFill />
-    </Button>
+    </RectButton>
   );
 };
 
