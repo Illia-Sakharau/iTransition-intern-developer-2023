e68a3b6989e1c726ba085ac36b18089ec06886e7
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-20 23:49:20 +0400

    feat: add errors range

--- task-5/src/modules/toolsBar/components/ErrorsInput.tsx
+++ task-5/src/modules/toolsBar/components/ErrorsInput.tsx
@@ -7,6 +7,7 @@ import { persons } from "../../../store/reducers/persons";
 
 const ErrorsInput: FC = () => {
   const errorInput = useRef<HTMLInputElement>();
+  const errorRange = useRef<HTMLInputElement>();
   const { errNum } = useAppSelector( state => state.personsParams );
   const dispatch = useAppDispatch();
   const { setErrNum } = personsParams.actions;
@@ -26,21 +27,30 @@ const ErrorsInput: FC = () => {
   }, 500)
 
   useEffect(() => {
-    if (errorInput.current) {
+    if (errorInput.current && errorRange.current) {
       errorInput.current.value = errNum.toString();
+      errorRange.current.value = errNum.toString()
     }
   },[errNum])
 
   return (
     <Stack direction="horizontal" gap={2}>
       <Form.Label className="text-white" htmlFor="seed">Errors:</Form.Label>
+      <Form.Range 
+        ref={errorRange as React.RefObject<HTMLInputElement>}
+        min={0}
+        max={10}
+        step={0.25}
+        defaultValue={errNum}
+        onChange={changeHandler}
+      />
       <Form.Control
         ref={errorInput as React.RefObject<HTMLInputElement>}
-        
         id="seed"
         type="number"
         min={0}
         max={1000}
+        step={0.25}
         defaultValue={errNum}
         onChange={changeHandler}
       />
