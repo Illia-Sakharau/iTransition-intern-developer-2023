3457723961a2aaac4b4f81f05b6bffc295bd8a1c
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-20 16:53:26 +0400

    init: start FE part

---
+++ task-5/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <>
+    <h1>Page</h1>
+    <Button>123</Button>
+    </>
+  )
+}
+
+export default App
