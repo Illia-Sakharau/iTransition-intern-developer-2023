8b91687846b6f4788841f687ada9af1f9a6a32df
Illia Sakharau <sakharovia0204@gmail.com>
2024-02-02 21:29:15 +0400

    feat: add join room event

--- index.js
+++ index.js
@@ -28,17 +28,26 @@ io.of('/T3').on('connection', (socket) => {
   socket.on('create_room', (username) => {
     const id = socket.id
     T3_ROOMS.push({ owner: username, id })
-    socket.broadcast.emit('all_rooms', {rooms: T3_ROOMS})
-    socket.join(id)
+    io.of('/T3').emit('all_rooms', {rooms: T3_ROOMS})
+    socket.join(`${id}`)
   })
 
   socket.on('close_room', (id) => {
     const roomIndex = T3_ROOMS.findIndex((room) => room.id === id)
     T3_ROOMS.splice(roomIndex, 1)
-    socket.broadcast.emit('all_rooms', {rooms: T3_ROOMS})
+    io.of('/T3').to(id).emit('room_closed')
+    io.of('/T3').emit('all_rooms', {rooms: T3_ROOMS})
   })
 
-  io.on('disconnect', () => {})
+  socket.on('join_room', ({ id, aponent }) => {
+    const roomIndex = T3_ROOMS.findIndex((room) => room.id === id)
+    T3_ROOMS[roomIndex].aponent = aponent
+    io.of('/T3').emit('all_rooms', {rooms: T3_ROOMS})
+    socket.join(id)
+    io.of('/T3').to(id).emit('aponent_joined', aponent)
+  })
+
+  io.of('/T3').on('disconnect', () => {})
 })
 
 io.of('/RPS').on('connection', (socket) => {
@@ -47,17 +56,26 @@ io.of('/RPS').on('connection', (socket) => {
   socket.on('create_room', (username) => {
     const id = socket.id
     RPS_ROOMS.push({ owner: username, id })
-    socket.broadcast.emit('all_rooms', {rooms: RPS_ROOMS})
-    socket.join(id)
+    io.of('/RPS').emit('all_rooms', {rooms: RPS_ROOMS})
+    socket.join(`${id}`)
   })
 
   socket.on('close_room', (id) => {
     const roomIndex = RPS_ROOMS.findIndex((room) => room.id === id)
     RPS_ROOMS.splice(roomIndex, 1)
-    socket.broadcast.emit('all_rooms', {rooms: RPS_ROOMS})
+    io.of('/RPS').to(id).emit('room_closed')
+    io.of('/RPS').emit('all_rooms', {rooms: RPS_ROOMS})
   })
 
-  io.on('disconnect', () => {})
+  socket.on('join_room', ({ id, aponent }) => {
+    const roomIndex = RPS_ROOMS.findIndex((room) => room.id === id)
+    RPS_ROOMS[roomIndex].aponent = aponent
+    io.of('/RPS').emit('all_rooms', {rooms: RPS_ROOMS})
+    socket.join(id)
+    io.of('/RPS').to(id).emit('aponent_joined', aponent)
+  })
+
+  io.of('/RPS').on('disconnect', () => {})
 })
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
